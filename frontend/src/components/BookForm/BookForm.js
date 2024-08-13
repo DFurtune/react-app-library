31881cf2ae0aafbe2690ae2e689baa10189c5d55
DFurtune <d.furtune18@gmail.com>
2024-08-13 15:40:28 +0300

    add book source

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -14,20 +14,15 @@ const BookForm = () => {
   const handleAddRandomBook = () => {
     const randomIndex = Math.floor(Math.random() * booksData.length);
     const randomBook = booksData[randomIndex];
-
-    const randomBookWithID = createBookWithID(randomBook);
-
+    const randomBookWithID = createBookWithID(randomBook, "random");
     dispatch(addBook(randomBookWithID));
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-
     if (title && author) {
-      const book = createBookWithID({ title, author });
-
+      const book = createBookWithID({ title, author }, "manual");
       dispatch(addBook(book));
-
       setTitle("");
       setAuthor("");
     }
@@ -37,7 +32,7 @@ const BookForm = () => {
     try {
       const res = await axios.get("http://localhost:4000/random-book");
       if (res?.data?.title && res?.data?.author) {
-        dispatch(addBook(createBookWithID(res.data)));
+        dispatch(addBook(createBookWithID(res.data, "API")));
       }
     } catch (error) {
       console.log("Error fetching random book");
